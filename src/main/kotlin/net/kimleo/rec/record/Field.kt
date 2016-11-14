8ae86e63edede329140513044dd7b8faa4078e95
Kimmy Leo <kenpusney@gmail.com>
2016-11-14 21:49:05 +0800

    Remove plus operator for Field and Record

--- src/main/kotlin/net/kimleo/rec/record/Field.kt
+++ src/main/kotlin/net/kimleo/rec/record/Field.kt
@@ -1,7 +1,3 @@
 package net.kimleo.rec.record
 
-data class Field(val value: String) {
-    operator fun plus(otherValue: String): Record {
-        return Record(listOf(this, Field(otherValue)))
-    }
-}
+data class Field(val value: String)
--- src/main/kotlin/net/kimleo/rec/record/Record.kt
+++ src/main/kotlin/net/kimleo/rec/record/Record.kt
@@ -1,12 +1,7 @@
 package net.kimleo.rec.record
 
 data class Record(val fields: List<Field>, val text: String) {
-
     val size = fields.size
 
     constructor(fields: List<Field>): this(fields, fields.map(Field::value).joinToString(", ")) {}
-
-    operator fun plus(value: String): Record {
-        return Record(fields + Field(value))
-    }
 }
