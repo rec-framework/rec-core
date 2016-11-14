fb259b0268a39e4883a7a03bb0aee9dc6363a5af
Kimmy Leo <kenpusney@gmail.com>
2016-11-14 22:08:04 +0800

    Make FieldSelector a data class

--- src/main/kotlin/net/kimleo/rec/repository/selector/FieldSelector.kt
+++ src/main/kotlin/net/kimleo/rec/repository/selector/FieldSelector.kt
@@ -3,7 +3,7 @@ package net.kimleo.rec.repository.selector
 import net.kimleo.rec.record.RecCollection
 import net.kimleo.rec.repository.RecRepository
 
-class FieldSelector(val type: String, val properties: List<String>) : Selector {
+data class FieldSelector(val type: String, val properties: List<String>) : Selector {
     override fun findAll(repo: RecRepository): List<RecCollection> {
         return listOf(repo.from(type).select(properties, "$type$properties"))
     }
--- src/test/kotlin/net/kimleo/rec/repository/selector/expr/SelectorParserTest.kt
+++ src/test/kotlin/net/kimleo/rec/repository/selector/expr/SelectorParserTest.kt
@@ -1,17 +1,20 @@
 package net.kimleo.rec.repository.selector.expr
 
 import net.kimleo.rec.repository.selector.FieldSelector
+import net.kimleo.rec.repository.selector.expr.SelectorParser.lex
+import net.kimleo.rec.repository.selector.expr.SelectorParser.parse
 import org.junit.Assert.*
 import org.junit.Test
 
 class SelectorParserTest {
     @Test
     fun testParser() {
-        val lex = SelectorParser.lex("cust, prod.world, i2b[name, pass], intj")
-        val parse = SelectorParser.parse(lex)
+        val lex = lex("cust, prod.hello world, i2b[name, pass], intj")
+        val parse = parse(lex)
 
         assertTrue(parse.size == 4)
         assertTrue((parse[1] as FieldSelector).properties.size == 1)
+        assertTrue((parse[1] as FieldSelector).properties[0] == "hello world")
         assertTrue((parse[2] as FieldSelector).properties.size == 2)
     }
 }
