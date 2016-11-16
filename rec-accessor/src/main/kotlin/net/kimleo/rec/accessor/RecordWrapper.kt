5470fdb8f2607dad04508268a5c4cfceda08a241
Kimmy Leo <kenpusney@gmail.com>
2016-11-16 23:08:14 +0800

    Indexible RecordWrapper

--- rec-accessor/src/main/kotlin/net/kimleo/rec/accessor/RecordWrapper.kt
+++ rec-accessor/src/main/kotlin/net/kimleo/rec/accessor/RecordWrapper.kt
@@ -2,14 +2,19 @@ package net.kimleo.rec.accessor
 
 import net.kimleo.rec.concept.Indexible
 
-data class RecordWrapper<T>(val fieldNames: Map<String, Int>, val record: Indexible<T>) {
-
+data class RecordWrapper<T>(val fieldNames: Map<String, Int>, val record: Indexible<T>): Indexible<T> {
     operator fun get(field: String): T? {
         if (fieldNames.containsKey(field)) {
             return getByIndex(fieldNames[field]!!, record)
         }
         return null
     }
+
+    override fun get(index: Int): T {
+        return record[index]
+    }
+
+    override val size = record.size
 
     private fun getByIndex(index: Int, record: Indexible<T>): T? {
         if (index >= 0) {
