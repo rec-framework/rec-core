0ec56bff3d7afa86021160ee7425c7df3f277d59
Kimmy Leo <kenpusney@gmail.com>
2016-12-14 18:07:15 +0800

    [#2] move kotlin version out

--- rec-app/src/main/kotlin/net/kimleo/rec/App.kt
+++ rec-app/src/main/kotlin/net/kimleo/rec/App.kt
@@ -4,10 +4,12 @@ import net.kimleo.rec.api.runscript
 import net.kimleo.rec.init.Initializer
 import net.kimleo.rec.loader.strategy.DefaultLoadingStrategy
 import net.kimleo.rec.rule.RuleLoader
+import net.kimleo.rec.scripting.runjs
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStreamReader
+import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -40,6 +42,17 @@ fun main(args: Array<String>) {
             val file = args[1]
             if (!File(file).exists()) { exit("File <$file> cannot be found!") }
             runscript(file)
+        }
+        "js" -> {
+            if (args.size != 2) {
+                exit("You need to provide the script file.")
+            }
+
+            val file = args[1]
+            val theFile = File(file)
+            if (!theFile.exists()) { exit("File <$file> cannot be found!") }
+
+            runjs(theFile.reader(Charset.defaultCharset()), theFile.name)
         }
         else -> {
             if (args.size != 1 || !Files.exists(Paths.get(args[0]))) {
