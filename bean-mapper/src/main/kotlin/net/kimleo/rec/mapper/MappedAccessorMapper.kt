8d64d116441a54c33932ffa5784f7d9d9180c8c5
Kimmy Leo <kenpusney@gmail.com>
2016-12-14 18:28:19 +0800

    [#2] remove bean-mapper project

--- bean-mapper/src/main/kotlin/net/kimleo/rec/mapper/MappedAccessorMapper.kt
+++
@@ -1,6 +0,0 @@
-package net.kimleo.rec.mapper
-
-import net.kimleo.rec.concept.Mapped
-
-interface MappedAccessorMapper<out T>: Mapper<Mapped<String>, T> {
-}
--- bean-mapper/src/main/kotlin/net/kimleo/rec/mapper/Mapper.kt
+++
@@ -1,5 +0,0 @@
-package net.kimleo.rec.mapper
-
-interface Mapper<in V, out T> {
-    fun map(mapped: V): T
-}
