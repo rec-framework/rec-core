eb9374a589a34e4fc6e40590d71f8bad54ac3092
Kimmy <kenpusney@gmail.com>
2018-12-10 23:32:24 +0800

    fix build

--- src/main/java/net/kimleo/rec/common/collection/LinkedMultiHashMap.java
+++ src/main/java/net/kimleo/rec/common/collection/LinkedMultiHashMap.java
@@ -1,6 +1,10 @@
 package net.kimleo.rec.common.collection;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
--- src/main/java/net/kimleo/rec/v2/utils/Persistence.java
+++ src/main/java/net/kimleo/rec/v2/utils/Persistence.java
@@ -1,6 +1,11 @@
 package net.kimleo.rec.v2.utils;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class Persistence {
     public static void saveObjectToFile(Serializable serializable, String file) throws IOException {
@@ -8,7 +13,8 @@ public class Persistence {
         stream.writeObject(serializable);
     }
 
-    public static <T extends Serializable> T loadObjectFromFile(String file) throws IOException, ClassNotFoundException {
+    public static <T extends Serializable> T loadObjectFromFile(String file)
+            throws IOException, ClassNotFoundException {
         ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
 
         return (T) in.readObject();
